65cfb4cf9f0c4a09fb2ca2ccecbb165c89ddcbe4
Luis Alves <luisfernandorsalves@gmail.com>
2021-12-28 17:52:03 -0600

    Watching for every message and printing it

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
-
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-# Install the Slack app and get xoxb- token in advance
-app = App(token=os.environ["SLACK_BOT_TOKEN"])
+# Initializes your app with your bot token and socket mode handler
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    # say(f"ASD there <@{message['user']}>!")
+    # for key, value in message.items():
+        # print(key, ' : ', value)
+    text = message['text']
+    say("The text is: " + text)
+
+# Start your app
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
