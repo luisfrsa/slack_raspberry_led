abaedfd20013f4916087ebcad79267254c902505
Luis Alves <luisfernandorsalves@gmail.com>
2022-01-07 18:29:22 -0600

    Modifying main script to connect to slack api via .env file

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import os
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from customer_actions import CustomerActions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# for customerActions in CustomerActions:
+#      print(customerActions)
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+APP_TOKEN = os.getenv('APP_TOKEN')
 
 # Initializes your app with your bot token and socket mode handler
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=BOT_TOKEN)
+
 
 # Listens to incoming messages that contain "hello"
 # To learn available listener arguments,
@@ -19,4 +30,4 @@ def message_hello(message, say):
 
 # Start your app
 if __name__ == "__main__":
-    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    SocketModeHandler(app, APP_TOKEN).start()
